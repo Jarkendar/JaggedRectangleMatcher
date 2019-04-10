ef2d369d2042ec5e4900f2a85d750959cadcc1e5
jaroslaw <jarek.skrzypczak@o2.pl>
2019-04-10 22:49:29 +0200

    Added read images

--- Main.py
+++ Main.py
@@ -1,9 +1,17 @@
 import sys
+from skimage import io
+
+IMAGE_SUFFIX = '.png'
+
+
+def readImages(path, numberOfImages):
+    return [io.imread(path + '/' + str(i) + IMAGE_SUFFIX) for i in range(numberOfImages)]
 
 
 def main():
     pathToImages = sys.argv[1]
-    imageNumber = sys.argv[2]
+    imageNumber = int(sys.argv[2])
+    readImages(pathToImages, imageNumber)
 
 
 if __name__ == '__main__':
