a573baabd775beb878a63de2e9519ac9466c6489
jaroslaw <jarek.skrzypczak@o2.pl>
2019-04-10 22:51:45 +0200

    Added method to show image used matplotlib

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import sys
 from skimage import io
+import matplotlib.pyplot as plt
 
 IMAGE_SUFFIX = '.png'
 
@@ -8,10 +9,15 @@ def readImages(path, numberOfImages):
     return [io.imread(path + '/' + str(i) + IMAGE_SUFFIX) for i in range(numberOfImages)]
 
 
+def showImage(image):
+    plt.imshow(image, cmap='gray', interpolation='nearest')
+    plt.show()
+
+
 def main():
     pathToImages = sys.argv[1]
     imageNumber = int(sys.argv[2])
-    readImages(pathToImages, imageNumber)
+    images = readImages(pathToImages, imageNumber)
 
 
 if __name__ == '__main__':
