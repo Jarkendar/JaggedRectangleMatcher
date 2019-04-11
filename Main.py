c3798b9956fc787792e1f19ac5b93f46e41e36d5
jaroslaw <jarek.skrzypczak@o2.pl>
2019-04-11 14:38:47 +0200

    Added count angle for 3 points from cosine theorem Added count angles for polygon

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import sys
-from skimage import io, measure, draw
+
 import matplotlib.pyplot as plt
-from numpy import zeros, shape, uint8
+from numpy import zeros, uint8, sqrt, arccos, pi as PI
+from skimage import io, measure, draw
 
 IMAGE_SUFFIX = '.png'
 POLYGON_TOLERANCE = 7
@@ -31,6 +32,18 @@ def findPolygon(image):
     return measure.approximate_polygon(bestContour, POLYGON_TOLERANCE)
 
 
+def countAngle(pLeft, pCenter, pRight):
+    a = (pCenter[0] - pLeft[0]) ** 2 + (pCenter[1] - pLeft[1]) ** 2
+    b = (pCenter[0] - pRight[0]) ** 2 + (pCenter[1] - pRight[1]) ** 2
+    c = (pRight[0] - pLeft[0]) ** 2 + (pRight[1] - pLeft[1]) ** 2
+    return arccos((a + b - c) / sqrt(4 * a * b)) * 180 / PI
+
+
+def countAngles(points):
+    return [countAngle(points[i - 1], points[i], points[i + 1] if i < len(points) - 1 else points[1])
+            for i in range(1, len(points))]
+
+
 def main():
     pathToImages = sys.argv[1]
     imageNumber = int(sys.argv[2])
@@ -38,7 +51,7 @@ def main():
 
     for i in images:
         polygon = findPolygon(i)
-        drawPolygon(polygon, shape(i))
+        angles = countAngles(polygon)
 
 
 if __name__ == '__main__':
