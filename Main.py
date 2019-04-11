354df0b2d2bc5858d6e2507e7eee93e044dda8d1
jaroslaw <jarek.skrzypczak@o2.pl>
2019-04-11 11:46:24 +0200

    Added find polygon method - from the best contour Added draw polygon - showing what has been found

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import sys
-from skimage import io
+from skimage import io, measure, draw
 import matplotlib.pyplot as plt
+from numpy import zeros, shape, uint8
 
 IMAGE_SUFFIX = '.png'
+POLYGON_TOLERANCE = 7
 
 
 def readImages(path, numberOfImages):
@@ -14,10 +16,29 @@ def showImage(image):
     plt.show()
 
 
+def drawPolygon(polygon, shape):
+    rr, cc = draw.polygon(polygon[:, 0], polygon[:, 1])
+    img = zeros(shape, dtype=uint8)
+    img[rr, cc] = 1
+    showImage(img)
+
+
+def findPolygon(image):
+    bestContour = None
+    for contour in measure.find_contours(image, 0):
+        if bestContour is None or len(bestContour) < len(contour):
+            bestContour = contour
+    return measure.approximate_polygon(bestContour, POLYGON_TOLERANCE)
+
+
 def main():
     pathToImages = sys.argv[1]
     imageNumber = int(sys.argv[2])
     images = readImages(pathToImages, imageNumber)
+
+    for i in images:
+        polygon = findPolygon(i)
+        drawPolygon(polygon, shape(i))
 
 
 if __name__ == '__main__':
